0e1c462bda403a35277b4448600f7a6ed033b337
Taeho <164999649+taeho89@users.noreply.github.com>
2026-05-29 15:58:02 +0900

    Revert "feat: add session creation feature"

--- src/main/java/com/groovo/server/session/repository/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.groovo.server.session.repository;
-
-import com.groovo.server.session.domain.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session, String> {
-}
